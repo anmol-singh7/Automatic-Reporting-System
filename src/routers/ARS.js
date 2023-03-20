0822dd293b57265f2f1be8bb06755ef48a8b122e
Anmol Singh <singhanmol0984@gmail.com>
2023-03-20 23:48:36 +0530

    make api to add data to CredentialMaster table ,UserMaster table ,RoleMaster table and to get data from SystemMaster and ManufacturerMaster and RoleMaster table

--- src/routers/ARS.js
+++ src/routers/ARS.js
@@ -3,6 +3,23 @@ const router = express.Router();
 const mysql = require('mysql2/promise');
 const { getConnection } = require('../db/connection');
 
+router.post('/addCredential', async (req, res) => {
+    try {
+        console.log("aaa")
+        const { clientid, hostofdatabase, userofdatabase, passwordofdatabase, databasename, waitForConnections, connectionLimit, queueLimit } = req.body;
+        console.log("bbb")
+        const connection = await getConnection();
+        console.log("ccc")
+        const query = "INSERT INTO CredentialMaster (clientid, hostofdatabase, userofdatabase, passwordofdatabase, databasename, waitForConnections, connectionLimit, queueLimit) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+        const [result] = await connection.query(query, [clientid, hostofdatabase, userofdatabase, passwordofdatabase, databasename, waitForConnections, connectionLimit, queueLimit]);
+        connection.release();
+        res.json({ message: "Credential added successfully!" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server Error' });
+    }
+});
+
 router.post('/addsystems', async (req, res) => {
     const { systemid, systemname, logoid } = req.body;
 
@@ -23,6 +40,18 @@ router.post('/addsystems', async (req, res) => {
     }
 });
 
+router.get('/systems', async (req, res) => {
+    try {
+        const connection = await getConnection();
+        const [rows] = await connection.query('SELECT * FROM SystemMaster');
+        connection.release();
+        res.json(rows);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server Error' });
+    }
+});
+
 router.post('/addmanufacturer', async (req, res) => {
     const { manufacturerid, manufacturername, logoid } = req.body;
 
@@ -37,6 +66,77 @@ router.post('/addmanufacturer', async (req, res) => {
         );
         connection.release();
         res.json({ message: 'System added successfully' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server Error' });
+    }
+});
+
+router.get('/manufacturers', async (req, res) => {
+    try {
+        const connection = await getConnection();
+        const [rows] = await connection.query('SELECT * FROM ManufacturerMaster');
+        connection.release();
+        res.json(rows);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server Error' });
+    }
+});
+
+router.post('/addusers', async (req, res) => {
+    const { userid, username, employid, department, usertype, phonenumber, email, password, userstatus } = req.body;
+
+    try {
+        const connection = await getConnection();
+        if (!userid || !username || !employid || !department || !usertype || !phonenumber || !email || !password || !userstatus) {
+            return res.status(400).json({ message: 'Invalid request' });
+        }
+        const result = await connection.query(
+            'INSERT INTO UserMaster (userid, username, employid, department,usertype,phonenumber, email, password,userstatus ) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
+            [userid, username, employid, department, usertype, phonenumber, email, password, userstatus]
+        );
+        connection.release();
+        res.json({ message: 'User added successfully' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server Error' });
+    }
+});
+
+router.post('/addRole', async (req, res) => {
+    const { roleid, rolename } = req.body;
+
+    try {
+        const connection = await getConnection();
+        const [existingRole] = await connection.query(
+            'SELECT * FROM RoleMaster WHERE roleid = ? OR rolename = ?',
+            [roleid, rolename]
+        );
+
+        if (existingRole.length > 0) {
+            return res.status(409).json({ message: 'Role already exists' });
+        }
+
+        const result = await connection.query(
+            'INSERT INTO RoleMaster (roleid, rolename) VALUES (?, ?)',
+            [roleid, rolename]
+        );
+
+        connection.release();
+        res.json({ message: 'Role added successfully' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server Error' });
+    }
+});
+
+router.get('/roles', async (req, res) => {
+    try {
+        const connection = await getConnection();
+        const [rows] = await connection.query('SELECT * FROM RoleMaster');
+        connection.release();
+        res.json(rows);
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: 'Server Error' });
