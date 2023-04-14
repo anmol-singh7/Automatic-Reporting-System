ae46dead062c5b127566e601b97dc5566646fc90
Anmol Singh <singhanmol0984@gmail.com>
2023-04-15 05:09:53 +0530

    modify description api to allow some input values to be empty and create an new api updateDescription

--- src/routers/ARS3.js
+++ src/routers/ARS3.js
@@ -207,7 +207,7 @@ router.post('/description', async (req, res) => {
     try {
         const connection = await getConnection();
         if (
-            !userid || !version || !clientid || !systems || !manufacturer || !datebegin || !timebegin || !dateend || !timeend || !timetype || !databasename || !table1 || !formtype || !status1 || !prechandler || !nexthandler || !count || !reportname) {
+            !userid || !clientid || !databasename || !table1 || !formtype) {
             return res.status(400).json({ message: 'Invalid request' });
         }
         // Generate the codegeneratedVianumberrep
@@ -1345,6 +1345,27 @@ router.patch('/deleteuser', async (req, res) => {
         }
 
         res.status(200).json({ message: `User with userid ${userid} updated to status inactive` });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server Error' });
+    }
+});
+
+router.patch('/updateDescription', async (req, res) => {
+    try {
+        const { reportid, systems, manufacturer, datebegin, dateend, timebegin, timeend, status1, reportname } = req.body;
+        const connection = await getConnection();
+        if (
+             !datebegin || !timebegin || !dateend || !timeend || !status1 || !reportname ) {
+            return res.status(400).json({ message: 'Invalid request' });
+        }
+        const query = "UPDATE DescriptionMaster SET systems = ?, manufacturer = ?, datebegin = ?, dateend = ?, timebegin = ?, timeend = ?, status1 = ?, reportname = ? WHERE reportid = ?";
+        const [result] = await connection.query(query, [systems, manufacturer, datebegin, dateend, timebegin, timeend, status1, reportname, reportid]);
+        connection.release();
+        if (result.affectedRows === 0) {
+            return res.status(404).json({ message: `Description with reportid ${reportid} not found` });
+        }
+        res.json({ message: `Description with reportid ${reportid} updated successfully` });
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: 'Server Error' });
